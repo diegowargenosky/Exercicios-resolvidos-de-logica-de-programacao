3d59bfd613bf2401600f6ce160118740c1e35255
Diego Wargenosky <diegoandrei22719@gmail.com>
2021-01-05 15:23:13 -0300

    Create consumo.java

---
+++ Estrutura_Sequencial/consumo.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+
+public class consumo {
+     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Fazer um programa para ler a distância total percorrida por um carro,bem como o total de combustível 
+        //gasto por este carro ao percorrer tal distância.Mostrar o consumo médio do carro com três casas decimais.
+
+ int distancia;
+double combustivelGasto, consumoMedio;
+
+System.out.print("Distância percorrida: ");
+  distancia = sc.nextInt();
+
+
+System.out.print("Combustível gasto: ");
+  combustivelGasto = sc.nextDouble();
+
+consumoMedio = (double) distancia / combustivelGasto;
+System.out.print("Consumo medio = " + String.format("%.3f", consumoMedio));
+
+        
+    }
+}
+
+    
