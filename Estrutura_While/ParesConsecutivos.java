260b5eb1ddbd3185e08ec6d8d54f1bf52a245bce
Diego Wargenosky <76791629+diegowargenosky@users.noreply.github.com>
2021-01-06 11:51:38 -0300

    Update ParesConsecutivos.java

--- Estrutura_While/ParesConsecutivos.java
+++ Estrutura_While/ParesConsecutivos.java
@@ -4,6 +4,9 @@ public class ParesConsecutivos {
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
+        
+ // O programa deve ler um valor inteiro X indefinidas vezes.(O programa irá parar quando o valor de X for igual a 0).
+ // Para cada X lido,imprima a soma dos 5 pares consecutivos a partir de X, inclusive o X se for par.
 
         System.out.print("Digite um numero inteiro: " );
   int X = sc.nextInt();
